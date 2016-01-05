bcb0fe76f2ae628435816603ceadab66a3f9fdd1
aanderson2323 <aanderson2323@gmail.com>
2016-01-04 20:47:27 -0600

    13.1

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -23,16 +23,14 @@ public class CrimeLab {
 
     private CrimeLab(Context context){
         mCrimes = new ArrayList <>();
-        for (int i = 0; i <100; i++){
-            Crime crime = new Crime();
-            crime.setTitle("Crime #" + i);
-            crime.setSolved(i%2 == 0); // Every other one
-            mCrimes.add(crime);
-        }
     }
 
     public List<Crime> getCrimes(){
         return mCrimes;
+    }
+
+    public void addCrime(Crime c){
+        mCrimes.add(c);
     }
 
     public Crime getCrime(UUID id){
@@ -43,4 +41,6 @@ public class CrimeLab {
         }
         return null;
     }
+
+
 }
